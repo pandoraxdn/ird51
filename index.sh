77614f3f9eebbe3bbb656ffaaa072a3ad8194264
pandoraxdn <pandoraxdn@protonmail.com>
2025-03-20 14:30:03 -0600

    Subí cambios

--- index.sh
+++ index.sh
@@ -2,7 +2,10 @@
 #!/bin/bash
 
 read -p "Ingresa el no. de proceso: " proceso
+read -p "Ingresa tu username: " username
 
 kill -9 $proceso
 
 echo "Se ha elimina el proceso: $proceso"
+
+echo "$username $USER" > .log_username
